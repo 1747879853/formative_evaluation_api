e88ffd7a7feeab61673e0e2b0a99f9159d009b35
caozhzh@office <caozhzh_qf@126.com>
2018-06-24 21:50:16 +0800

    ruby version 2.3.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,3 +38,6 @@ append :linked_dirs, "public/_attachment"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+# rvm setting
+set :rvm_ruby_version, '2.3.1'
