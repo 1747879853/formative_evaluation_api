cf1d3b4887ffa2b287d9f8d92a3e1e6231fc6e6e
caozhzh@office <caozhzh_qf@126.com>
2018-06-24 18:04:58 +0800

    repo url changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "wisdom_secretary_api"
-set :repo_url, 'ssh://passenger@127.0.0.1/home/passenger/wisdomSecretary.git'
+set :repo_url, 'ssh://passenger@127.0.0.1/home/passenger/wisdom_secretary_api.git'
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
