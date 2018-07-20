6c841d30e77bbcc2c5b3de32e67672fafb812481
caozhzh <caozhzh@qfnu.edu.cn>
2018-07-20 20:19:00 +0800

    unauthorized return 403 forbidden to different with 401 unauthorized

--- app/controllers/api/v1/authorities_controller.rb
+++ app/controllers/api/v1/authorities_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::AuthoritiesController < Api::V1::BaseController
   # before_action :authorize
   
   def get_rulelist
-    # unauthorized and return unless Auth.check('Admin/Authority/list', current_user)
+    unauthorized and return unless Auth.check('Admin/Authority/list', current_user)
 
     render json: AuthRule.where(parent_id: 0).all
   end
