070068c52df0fbfd0b1d48af1ed0dc07b6bcd575
yyk <yyk@zpy.com>
2018-09-26 10:40:18 +0800

    # rename_column Costdata summary_id

--- db/migrate/20180925064206_rename_column_of_costdatas.rb
+++ db/migrate/20180925064206_rename_column_of_costdatas.rb
@@ -3,7 +3,7 @@ class RenameColumnOfCostdatas < ActiveRecord::Migration[5.2]
   	
     # add_column :schedules, :type, :integer, default: 0
     # rename_column :schedules, :type, :schedule_type
-    rename_column :costdata, :summaries_id, :summary_id
+    # rename_column :costdata, :summaries_id, :summary_id
     add_column :costdata, :item_third_id, :integer
   end
 end
