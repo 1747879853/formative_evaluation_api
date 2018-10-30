4e48ee10fd4d8ff0a92aa76684bb305ac4240735
caozhzh <caozhzh@gmail.com>
2018-10-30 18:14:20 +0800

    delete workteam from user_token_controller

--- app/controllers/api/v1/user_token_controller.rb
+++ app/controllers/api/v1/user_token_controller.rb
@@ -6,7 +6,6 @@ class Api::V1::UserTokenController < Knock::AuthTokenController
   	h = {
   		"jwt": json["token"],
   		"auth_rules": entity.nil? ? [] : entity.auth_rules.active.select(:name).map(&:name),
-  		"banzu": WorkTeam.find_by_user_id(json["payload"]["sub"]) ? true : false
   	}
   	render json: h, status: :created
   end
