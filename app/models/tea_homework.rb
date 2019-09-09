519bab62f68cd26bb768fa27df9466d442ac94f2
yyk <yyk@example.com>
2019-09-09 17:10:07 +0800

    tea home work settime from date to datetime

--- app/models/tea_homework.rb
+++ app/models/tea_homework.rb
@@ -1,10 +1,9 @@
 class TeaHomework < ApplicationRecord
 
- #  def as_json(options = {})
-	# h = super(options)	
-	# h[:title] = self.name
-	# h[:checked] = false
-	# h[:checked_id] = evaluations.map(&:id)
-	# h
- #  end
+  def as_json(options = {})
+	h = super(options)	
+	h[:start_time] = self.start_time.strftime('%Y-%m-%d %H:%M:%S')
+	h[:end_time] = self.end_time.strftime('%Y-%m-%d %H:%M:%S')
+	h
+  end
 end
