1a0357e19f077e1835544bc32cb8cd25a8bdcba4
yyd <1747879853@qq.com>
2019-11-12 20:20:07 +0800

    调试bug

--- app/controllers/api/v1/class_grade_input_controller.rb
+++ app/controllers/api/v1/class_grade_input_controller.rb
@@ -408,25 +408,25 @@ class Api::V1::ClassGradeInputController < Api::V1::BaseController
     times = Grade.where("class_rooms_id IN (#{checked_class_rooms_id})").where("courses_id IN (#{checked_courses_id})").where(term:checked_term)
     res = []
     t = {}
-    times.each do |i|
-      if checked_teachers_id.include? TeachersClassesCourse.select(:teachers_id).where(class_rooms_id: i.class_rooms_id,courses_id: i.courses_id,term: checked_term).first.teachers_id
-        t["teachers_name"] = Teacher.find(TeachersClassesCourse.select(:teachers_id).where(class_rooms_id: i.class_rooms_id,courses_id: i.courses_id,term: checked_term).first.teachers_id).name
-        t["students_name"] = Student.find(i.students_id).name
-        t["students_num"] = Student.find(i.students_id).sno
-        t["course_name"] = Course.find(i.courses_id).name
-        t["class_rooms_name"] = ClassRoom.find(i.class_rooms_id).name
-        t["grade"] = Grade.find(i.id).grade
+    #times.each do |i|
+    #  if checked_teachers_id.include? TeachersClassesCourse.select(:teachers_id).where(class_rooms_id: i.class_rooms_id,courses_id: i.courses_id,term: checked_term).first.teachers_id
+    #    t["teachers_name"] = Teacher.find(TeachersClassesCourse.select(:teachers_id).where(class_rooms_id: i.class_rooms_id,courses_id: i.courses_id,term: checked_term).first.teachers_id).name
+    #    t["students_name"] = Student.find(i.students_id).name
+    #    t["students_num"] = Student.find(i.students_id).sno
+    #    t["course_name"] = Course.find(i.courses_id).name
+    #    t["class_rooms_name"] = ClassRoom.find(i.class_rooms_id).name
+    #    t["grade"] = Grade.find(i.id).grade
         
-        t["description"] = Evaluation.find(i.evaluations_id).description
-        if Evaluation.find(i.evaluations_id).parent_id == 0
-          t["evaluations"] = Evaluation.find(i.evaluations_id).name
-        else
-          t["evaluations"] = Evaluation.find(Evaluation.find(i.evaluations_id).parent_id).name+"-"+Evaluation.find(i.evaluations_id).name
-        end
-        res.push t
-        t = {}
-      end
-    end
+    #    t["description"] = Evaluation.find(i.evaluations_id).description
+    #    if Evaluation.find(i.evaluations_id).parent_id == 0
+     #     t["evaluations"] = Evaluation.find(i.evaluations_id).name
+     #   else
+     #     t["evaluations"] = Evaluation.find(Evaluation.find(i.evaluations_id).parent_id).name+"-"+Evaluation.find(i.evaluations_id).name
+     #   end
+      #  res.push t
+      #  t = {}
+      #end
+    #end
       
     #res = Grade.select("grades.grade,students.name,courses.name,class_rooms.name,evaluations.name,evaluations.description").joins("inner join teachers on teachers_classes_courses.teachers_id=teachers.id) inner join students on grades.students_id = students.id inner join courses on grades.courses_id=courses.id inner join class_rooms on grades.class_rooms_id=class_rooms.id inner join evaluations on grades.evaluations_id=evaluations.id")
 
