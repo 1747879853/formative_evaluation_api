99f296cbaa64ef1617147ae6812384fe346a1527
yyd <1747879853@qq.com>
2020-01-14 20:56:42 +0800

    fix bug

--- app/controllers/api/v1/class_grade_input_controller.rb
+++ app/controllers/api/v1/class_grade_input_controller.rb
@@ -469,6 +469,7 @@ class Api::V1::ClassGradeInputController < Api::V1::BaseController
     student_grade_list = []
     evaluations_weight = []
     b = {}
+    ee = ''
     evaluations_id_falg = []
     term_id = params[:term]
     class_room_id = params[:class_room_id]
@@ -480,10 +481,10 @@ class Api::V1::ClassGradeInputController < Api::V1::BaseController
         
         
         evaluations_weight.push Evaluation.where(id: j.evaluations_id)
-      #  ee = evaluations_weight[1].parent_id
+       ee = evaluations_weight[1].parent_id
       end
     end
-    render json: {'a': evaluations_weight}
+    render json: {'a': evaluations_weight,'ee':ee}
     rescue Exception => e
       render json: { msg: e }, status: 500      
     end
