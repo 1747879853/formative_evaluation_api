6db05316c08511e43391d98758768c9018555a9a
yyd <1747879853@qq.com>
2020-01-14 21:02:58 +0800

    fix bug

--- app/controllers/api/v1/class_grade_input_controller.rb
+++ app/controllers/api/v1/class_grade_input_controller.rb
@@ -484,7 +484,7 @@ class Api::V1::ClassGradeInputController < Api::V1::BaseController
        
       end
     end
-    ee = evaluations_weight[1]
+    ee = evaluations_weight[1][0].parent_id
     render json: {'a': evaluations_weight,'ee':ee}
     rescue Exception => e
       render json: { msg: e }, status: 500      
