5e5b504967b210355e077d19e8deb6eda07cf7c0
yyd <1747879853@qq.com>
2020-01-15 01:16:14 +0800

    fix bug

--- app/controllers/api/v1/class_grade_input_controller.rb
+++ app/controllers/api/v1/class_grade_input_controller.rb
@@ -485,12 +485,12 @@ class Api::V1::ClassGradeInputController < Api::V1::BaseController
           parent_id1 = Evaluation.where(id: j.evaluations_id)
         end
         b[:parent_id_b] = parent_id1
-        b[:weight] = 0
+        b[:weight] = 1
         student_grade_list.each do |k|
-            if Evaluation.where(id: k.evaluations_id).first.parent_id == parent_id1 && !(evaluations_id_falg.include? k.evaluations_id)
-              b[:weight] +=  Weight.where(evaluations_id:k.evaluations_id).where(courses_id:course_id).first.weight.to_f
-              evaluations_id_falg.push k.evaluations_id
-            end
+            #if Evaluation.where(id: k.evaluations_id).first.parent_id == parent_id1 && !(evaluations_id_falg.include? k.evaluations_id)
+            #  b[:weight] +=  Weight.where(evaluations_id:k.evaluations_id).where(courses_id:course_id).first.weight.to_f
+            #  evaluations_id_falg.push k.evaluations_id
+           # end
         end
         if b['weight'] != 0 
           evaluations_weight.push b
