9a35d19c6a993abbb40c51e0e45f250b04114468
yyd <1747879853@qq.com>
2020-01-15 09:51:52 +0800

    fix bug

--- app/controllers/api/v1/class_grade_input_controller.rb
+++ app/controllers/api/v1/class_grade_input_controller.rb
@@ -622,7 +622,7 @@ class Api::V1::ClassGradeInputController < Api::V1::BaseController
      student_score_end.push b
      b = {}
     end
-    render json: {'a': student_score_midle,'b': evaluations_weight,'c': test_}
+    render json: {'a': student_score_end,'b': evaluations_weight,'c': test_}
     rescue Exception => e
       render json: { msg: e }, status: 500      
     end
