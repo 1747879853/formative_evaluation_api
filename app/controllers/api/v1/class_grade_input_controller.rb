8cc0162bc4cd226b50625b25df721e9af83d89cb
yyd <1747879853@qq.com>
2020-01-15 18:14:36 +0800

    fix bug

--- app/controllers/api/v1/class_grade_input_controller.rb
+++ app/controllers/api/v1/class_grade_input_controller.rb
@@ -496,8 +496,8 @@ class Api::V1::ClassGradeInputController < Api::V1::BaseController
          parents_flag.push eva.parent_id
           eva_list = Evaluation.where(parent_id: eva.parent_id)
           eva_list.each do |z|
-            uuu.push z
-       #     eva2 = Evaluation.where(id: z.evaluation_id).first
+            uuu.push z.evaluation_id
+            eva2 = Evaluation.where(id: z.evaluation_id).first
        #    uuu.push eva2
         #    if eva2 == nil
         #      next
